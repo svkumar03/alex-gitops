2a35718938ef1de127024644da1c6975f946436c
svkumar03 <60556436+svkumar03@users.noreply.github.com>
2020-02-23 16:47:17 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 # Terraform state will be stored in S3
 terraform {
   backend "s3" {
-    bucket = "terraform-bucket-alex"
+    bucket = "terraform-bucket-venkat"
     key    = "terraform.tfstate"
     region = "us-east-1"
   }
